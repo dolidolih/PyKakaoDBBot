2e5f1b81a28390e99e4fc273415136825dec21ab
dolidolih <130177574+dolidolih@users.noreply.github.com>
2023-04-09 14:52:36 +0900

    modified send function

--- socketsender.py
+++ socketsender.py
@@ -19,15 +19,5 @@ def send_via_socket(is_success,type,data,room,msg_json):
     clientSocket.send(json.dumps(res).encode("utf-8"))
     clientSocket.close()
 
-def s(msg,room):
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((ip,port))
-
-    res = { "isSuccess":True,
-        "type":"normal",
-        "data":base64.b64encode(msg.encode()).decode(),
-        "room":base64.b64encode(room.encode()).decode(),
-        "msgJson":base64.b64encode('[]'.encode()).decode()}
-
-    clientSocket.send(json.dumps(res).encode("utf-8"))
-    clientSocket.close()
+def send(msg,room):
+    send_via_socket(True,"normal",msg,room,{})
