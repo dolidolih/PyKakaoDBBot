0f7d06791c5912734c3f834fe73ab776763d9811
dolidolih <gagakagaga1@gmail.com>
2023-04-24 21:44:00 +0900

    removed commands config, added db to response

--- chatbot/Response.py
+++ chatbot/Response.py
@@ -1,4 +1,4 @@
-def response(room, msg, sender, replier, msg_json):
+def response(room, msg, sender, replier, msg_json, db):
     print(msg_json)
     if msg == "!hi":
         replier.reply("hello")
--- helper/ObserverHelper.py
+++ helper/ObserverHelper.py
@@ -9,13 +9,6 @@ class ObserverHelper:
         self.BOT_ID = self.config["bot_id"]
         self.BOT_NAME = self.config["bot_name"]
 
-    def is_command(self, msg):
-        commands = get_config()["commands"]
-        if msg.split(' ')[0] in commands:
-            return True
-        else:
-            return False
-
     def make_post_data(self, dec_msg, room, sender, js):
         data = {"msg" : dec_msg,
                 "room" : room,
@@ -42,16 +35,15 @@ class ObserverHelper:
                 enc_msg = row[5]
                 user_id = row[4]
                 dec_msg = KakaoDecrypt.decrypt(enc,enc_msg,user_id)
-                if self.is_command(dec_msg):
-                    chat_id = row[3]
-                    user_info = db.get_user_info(chat_id,user_id)
-                    room = user_info[0]
-                    sender = user_info[1]
-                    if room == self.BOT_NAME:
-                        room = sender
-                    post_data = self.make_post_data(dec_msg, room, sender, {description[i]:row[i] for i in range(len(row))})
-                    requests.post("http://127.0.0.1:5000/db",data={"data":post_data})
-                    print('sent')
+                chat_id = row[3]
+                user_info = db.get_user_info(chat_id,user_id)
+                room = user_info[0]
+                sender = user_info[1]
+                if room == self.BOT_NAME:
+                    room = sender
+                post_data = self.make_post_data(dec_msg, room, sender, {description[i]:row[i] for i in range(len(row))})
+                requests.post("http://127.0.0.1:5000/db",data={"data":post_data})
+                print('sent')
 
 def get_config():
     with open('config.json','r') as fo:
