30f6c8dbf683848e16aab6010680e0d9ad95e502
dolidolih <gagakagaga1@gmail.com>
2023-04-25 16:30:34 +0900

    modified inheritance

--- chatbot/Response.py
+++ chatbot/Response.py
@@ -1,3 +1,7 @@
 def response(room, msg, sender, replier, msg_json, db):
     if msg == "!hi":
         replier.reply("hello")
+    elif msg == "!d":
+        replier.reply(db.DB_PATH)
+    elif msg == "!기능":
+        replier.reply("없어요")
