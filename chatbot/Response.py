f023a4945e0eccda7206789b8d55bc7bcde6bb07
dolidolih <gagakagaga1@gmail.com>
2023-04-25 17:08:10 +0900

    fixed bug for non openchat

--- chatbot/Response.py
+++ chatbot/Response.py
@@ -1,7 +1,3 @@
 def response(room, msg, sender, replier, msg_json, db):
     if msg == "!hi":
         replier.reply("hello")
-    elif msg == "!d":
-        replier.reply(db.DB_PATH)
-    elif msg == "!기능":
-        replier.reply("없어요")
