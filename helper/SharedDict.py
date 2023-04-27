441e1e2f0c2f1ebf8f9d8ac34f26fb3b66297af1
dolidolih <gagakagaga1@gmail.com>
2023-04-27 23:12:26 +0900

    added comment

--- helper/SharedDict.py
+++ helper/SharedDict.py
@@ -1,5 +1,7 @@
 from multiprocessing.managers import AcquirerProxy, BaseManager, DictProxy
 
+
+#https://stackoverflow.com/questions/57734298/how-can-i-provide-shared-state-to-my-flask-app-with-multiple-workers-without-dep
 def get_shared_state():
     shared_dict = {}
     manager = BaseManager(("127.0.0.1", 35791), b"pykakao")
